fe46b449ba8a3689b9e5a25d43fe2be6ea2e72ad
Nitish Madhukar <nitishmadhukar@gmail.com>
2017-03-12 23:05:51 -0400

    Adds controller to list and destroy contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'contacts#index'
+  resources :contacts, only: [:index, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
