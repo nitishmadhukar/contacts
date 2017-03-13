3db617219bf3154b0250d1922e98b405cbe77423
Nitish Madhukar <nitishmadhukar@gmail.com>
2017-03-12 21:08:33 -0400

    Generates Contact model with corresponding validations

---
+++ db/migrate/20170311214328_create_contacts.rb
@@ -0,0 +1,14 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email_address, index: true
+      t.string :phone_number
+      t.string :extension
+      t.string :company_name
+
+      t.timestamps null: false
+    end
+  end
+end
