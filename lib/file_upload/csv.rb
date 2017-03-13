3bc65c756a5882ba0759bb4313c9559e463b8804
Nitish Madhukar <nitishmadhukar@gmail.com>
2017-03-13 00:57:15 -0400

    Adds file upload helpers

---
+++ lib/file_upload/csv.rb
@@ -0,0 +1,21 @@
+require 'csv'
+
+module FileUpload
+  class Csv
+    def initialize(file)
+      @file = file
+    end
+
+    def parse
+      data = []
+      CSV.foreach("data.csv",
+        headers: true,
+        converters: :all,
+        header_converters: lambda { |column| column.downcase.gsub(' ', '_') }
+      ) do |row|
+          data << row.to_hash
+      end
+      return data
+    end
+  end
+end
