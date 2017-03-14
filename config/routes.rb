ed44e254a70a359b30e46bac972b6aea1f190bfe
Nitish Madhukar <nitishmadhukar@gmail.com>
2017-03-13 22:40:30 -0400

    Deletes multiple contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'contacts#index'
   resources :contacts, only: [:index, :destroy]
   post 'contacts/upload'
+  post 'contacts/destroy_multiple'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
